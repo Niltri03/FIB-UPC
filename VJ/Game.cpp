fd213cc2b881b1b8a34c9042000fd8eb04c8a6f8
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-11-06 14:22:24 +0100

    Add files via upload

---
+++ VJ/Game.cpp
@@ -0,0 +1,99 @@
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include "Game.h"
+
+
+void Game::init()
+{
+	bPlay = true;
+	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+	scene.init1();
+}
+
+bool Game::update(int deltaTime)
+{
+	scene.update(deltaTime);
+	
+	return bPlay;
+}
+
+void Game::render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	scene.render();
+}
+
+void Game::keyPressed(int key)
+{
+	if(key == 27) // Escape code
+		bPlay = false;
+	if(key==1){
+		if(((curScene!="instr" or curScene!="creds")){scene.init1(); 
+		curScene = "lv1"; }
+	}
+	if(key==2){
+		if(curScene=="main"){
+			scene.initInstr(); 
+			curScene="instr"; 
+		}
+		else if(curScene=="lv1"){
+			scene.init2(); 
+			curScene = "lv2";
+		}
+	}
+	if(key=="b"){
+		if(curScene=="instr" or curScene=="creds"){
+			scene.initMain();
+			curScene = "main"; 
+		}
+	}
+	if(key==3){
+		if((curScene!="lv1")and(curScene!="lv2")){
+			scene.initCredits();
+			curScene="creds";
+		}
+	}
+	keys[key] = true;
+}
+
+void Game::keyReleased(int key)
+{
+	keys[key] = false;
+}
+
+void Game::specialKeyPressed(int key)
+{
+	specialKeys[key] = true;
+}
+
+void Game::specialKeyReleased(int key)
+{
+	specialKeys[key] = false;
+}
+
+void Game::mouseMove(int x, int y)
+{
+}
+
+void Game::mousePress(int button)
+{
+}
+
+void Game::mouseRelease(int button)
+{
+}
+
+bool Game::getKey(int key) const
+{
+	return keys[key];
+}
+
+bool Game::getSpecialKey(int key) const
+{
+	return specialKeys[key];
+}
+
+
+
+
+
