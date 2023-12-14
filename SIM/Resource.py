e7b7d2cab09e294927e7d103cb81b60e0a93949b
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-12-14 17:35:30 +0100

    Resourcve

--- SIM/Resource.py
+++ SIM/Resource.py
@@ -1,5 +1,13 @@
 from slam import *
 
+'''
+CLASE Resource 
+DOS ESTADOS: "FREE" o "BUSY" (changeState) 
+FUNCIONES: 
+tractarEsdeveniment(resource, eventoATratar)
+
+'''
+
 class resource(slamiii):
 
     def __init__(self,scheduler,parametres):
@@ -7,9 +15,26 @@ class resource(slamiii):
         #Que faig amb parametres, en aquest cas nop
         self.state="FREE"
 
-    def changeState(self, newState):
-        self.state == newState
+    #Recibe un evento, un Resource( a si mismo ) y devuelve ¿?XD dependiendo del estado actual.
+    def tractarEsdeveniment(self, event):
+        if(self.state == "FREE"):
+            self.state == "BUSY"
+            #hago codigo
+            self.state == "FREE-"
+        else:
+            pass
+        pass
+
+
+    def changeState(self, newState):#Primer parametro el objeto a cambiar el estado, segundo parametro el nuevo estado
+        if(newState == "FREE" or newState == "BUSY"):
+            self.state == newState
+        else:
+            print("Not a valid state")
 
     def acceptaEntitat(self, n):
         return n
 
+    def fiSimulacio(self):
+        #Aquí tampoc faig res
+        del self
