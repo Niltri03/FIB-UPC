af6dac838f40a5daed429cd6c9e785f5c7c53f25
Nacho F <119792167+Niltri03@users.noreply.github.com>
2023-12-21 16:30:38 +0100

    Update Resource.py

--- SIM/Resource.py
+++ SIM/Resource.py
@@ -5,6 +5,9 @@ CLASE Resource
 DOS ESTADOS: "FREE" o "BUSY" (changeState) 
 FUNCIONES: 
 tractarEsdeveniment(resource, eventoATratar)
+me comunico con alter prempt free
+Diagrama de grafo: Flechas son funciones/signals, nodos son estados
+
 
 '''
 
@@ -13,14 +16,19 @@ class resource(slamiii):
     def __init__(self,scheduler,parametres):
         super(resource, self).__init__(scheduler,parametres)
         #Que faig amb parametres, en aquest cas nop
+        self.parametres = parametres
+        atributos = parametres.split(',')
         self.state="FREE"
+
+    def __repr__(self):
+        return "resource"
 
     #Recibe un evento, un Resource( a si mismo ) y devuelve ¿?XD dependiendo del estado actual.
     def tractarEsdeveniment(self, event):
         if(self.state == "FREE"):
             self.state == "BUSY"
             #hago codigo
-            self.state == "FREE-"
+            self.state == "FREE"
         else:
             pass
         pass
@@ -29,12 +37,19 @@ class resource(slamiii):
     def changeState(self, newState):#Primer parametro el objeto a cambiar el estado, segundo parametro el nuevo estado
         if(newState == "FREE" or newState == "BUSY"):
             self.state == newState
-        else:
-            print("Not a valid state")
+
+    def entraAlter(self):
 
     def acceptaEntitat(self, n):
         return n
 
+    def iniciSimulacio(self):
+        pass
+
+    def summary(self):
+        pass
+        #nº entidades procesadas 
+    
     def fiSimulacio(self):
         #Aquí tampoc faig res
         del self
